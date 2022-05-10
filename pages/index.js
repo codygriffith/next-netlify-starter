69e289fc815d927fc0bd148fda2ca2fba3b777cb
Cody Griffith <cody@alenthea.com>
2022-05-09 22:24:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div className="container">
+      <Script
+        id="netlify-id"
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+        
+      />
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,6 +21,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <div data-netlify-identity-menu></div>
       </main>
 
       <Footer />
